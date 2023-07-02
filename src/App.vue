adb837f9878115b9bbf2ee369dd064de143851f0
wzd031120 <2401176210@qq.com>
2023-07-02 09:39:08 +0800

    初始化前端第一次弄

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import Edit from './components/Edit.vue'
+import {onMounted, ref} from "vue";
+import axios from 'axios'
+
+// TODO: 列表渲染
+const List = ref([])
+const GetList = async () => {
+  const res = await axios.get('/list')
+  List.value =res.data
+}
+onMounted(()=>GetList())
+// TODO: 删除功能
+
+
+// TODO: 编辑功能
+
+</script>
+
+<template>
+  <div class="app">
+    <el-table :data="List">
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="姓名" prop="name" width="150"></el-table-column>
+      <el-table-column label="籍贯" prop="place"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default>
+          <el-button type="primary" link>编辑</el-button>
+          <el-button type="danger" link>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+  <Edit />
+</template>
+
+<style scoped>
+.app {
+  width: 980px;
+  margin: 100px auto 0;
+}
+</style>
